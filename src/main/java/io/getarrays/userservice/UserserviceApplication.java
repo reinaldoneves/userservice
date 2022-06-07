d6b1c61e85689a153e8d44b75a1619aa6f34a923
reinaldoneves <reinaldo_neves@hotmail.com>
2022-06-07 16:04:57 -0300

    saveUser, saveRole and addRoleToUser - Working fine!

--- src/main/java/io/getarrays/userservice/UserserviceApplication.java
+++ src/main/java/io/getarrays/userservice/UserserviceApplication.java
@@ -1,7 +1,14 @@
 package io.getarrays.userservice;
 
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -10,6 +17,30 @@ public class UserserviceApplication {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
-	//TODO: Continua daqui parça: https://youtu.be/VVn9OG9nfH0?t=2052
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+
+			userService.saveRole(new Role(null,"ROLE_USER"));
+			userService.saveRole(new Role(null,"ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "Pixinguinha","pixinguinha", "123456", new ArrayList<>()));
+			userService.saveUser(new User(null, "Moacir Santos","moacir_santos", "123456", new ArrayList<>()));			userService.saveUser(new User(null, "User full name","username", "password", new ArrayList<>()));
+			userService.saveUser(new User(null, "Garoto","garoto", "123456", new ArrayList<>()));			userService.saveUser(new User(null, "User full name","username", "password", new ArrayList<>()));
+			userService.saveUser(new User(null, "Baden Powell","baden_powell", "123456", new ArrayList<>()));
+
+			userService.addRoleToUser("pixinguinha", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("moacir_santos", "ROLE_USER");
+			userService.addRoleToUser("moacir_santos", "ROLE_MANAGER");
+			userService.addRoleToUser("garoto", "ROLE_MANAGER");
+			userService.addRoleToUser("baden_powell", "ROLE_USER");
+			userService.addRoleToUser("baden_powell", "ROLE_ADMIN");
+
+		};
+	}
+
+	//TODO: Continua daqui parça: https://youtu.be/VVn9OG9nfH0?t=2855
 
 }
