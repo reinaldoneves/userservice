5421d1f4fbcce5bffbd4f86c3b21c9f9990bf80d
reinaldoneves <reinaldo_neves@hotmail.com>
2022-06-07 10:42:22 -0300

    application.properties file config

--- src/main/java/io/getarrays/userservice/resources/UserResource.java
+++ src/main/java/io/getarrays/userservice/resources/UserResource.java
@@ -1,12 +1,25 @@
 package io.getarrays.userservice.resources;
 
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("*/api")
 @RequiredArgsConstructor
 public class UserResource {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
 
 }
